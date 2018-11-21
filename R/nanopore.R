b78f250ee327698697c785784e8e56201cc0f770
Christian Diener <ch.diener@gmail.com>
2018-11-21 09:40:14 -0800

    fix nanopore alignments

--- R/nanopore.R
+++ R/nanopore.R
@@ -11,7 +11,7 @@
 #' @param log_file The log file used to store status messages.
 #' @param threads Number of threds used for alignment.
 #' @param secondary Return at most that many secondary alignments.
-#' @return A data.table with sequence names, counts and sample name.
+#' @return A data.table with sample name and alignment.
 #'
 #' @export
 align_nanopore <- function(read_files, ref, alignments_folder = "./alignments",
@@ -21,7 +21,7 @@ align_nanopore <- function(read_files, ref, alignments_folder = "./alignments",
         dir.create(alignments_folder)
     }
     flog.info("Aligning reads to 16S references")
-    successes <- lapply(read_files, function(file) {
+    alns <- lapply(read_files, function(file) {
         flog.info("Aligning %s...", file)
         base <- strsplit(basename(file), ".fa")[[1]][1]
         out_path <- file.path(alignments_folder,
@@ -30,9 +30,13 @@ align_nanopore <- function(read_files, ref, alignments_folder = "./alignments",
         args <- append(args, c(paste0("2>", log_file), "|", "samtools",
                             "view", "-bS", "-", ">", out_path))
         success <- system2("minimap2", args = args)
-        return(success)
+        return(data.table(sample = base, alignment = out_path,
+               success = success == 0))
     })
-    if (any(successes != 0)) {
+    alns <- rbindlist(alns)
+    if (alns[, any(!success)]) {
         stop("At least one alignment failed!")
     }
+    alns[, success := NULL]
+    return(alns)
 }
