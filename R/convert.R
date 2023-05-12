347938e5db0b3c35a1962772cd73b4adc72acd15
Christian Diener <ch.diener@gmail.com>
2023-05-12 14:00:02 -0700

    add some helpers

--- R/convert.R
+++ R/convert.R
@@ -44,3 +44,107 @@ mothur_to_dada <- function(seq_file, taxa_file, out = "taxonomy.fa.gz") {
     seqs <- ShortRead(sread(seqs), BStringSet(tax[ids]))
     writeFasta(seqs, out, compress = TRUE)
 }
+
+#' Converts BRACKEN results to a phyloseq object.
+#'
+#' @param bracken A data.table containing the BRACKEN quantifications.
+#' @param metadata A metadata in data.table format to use.
+#' @param id_col Column in the metdata corresponding to the sample IDs
+#' in the BRACKEN counts.
+#' @return A phyloseq object for the data.
+#' @examples
+#'  NULL
+#'
+#' @export
+#' @importFrom data.table setkeyv
+bracken_to_phyloseq <- function(
+    bracken,
+    metadata = NULL,
+    id_col = "sample_id") {
+
+    n_ranks <- ncol(bracken) - 2
+    lowest_rank <- names(bracken)[n_ranks]
+    table <- dcast(
+        bracken,
+        reformulate(lowest_rank, response = "sample"),
+        value.var = "reads",
+        fill = 0
+    )
+    samps <- table[, sample]
+    table <- as.matrix(table[, !"sample"])
+    rownames(table) <- samps
+    taxa <- unique(bracken[, 1:n_ranks, with = FALSE])
+    setkeyv(taxa, lowest_rank)
+    taxa <- as.matrix(taxa[colnames(table)])
+    rownames(taxa) <- taxa[, lowest_rank]
+    colnames(taxa) <- names(bracken)[1:n_ranks]
+
+    if (is.null(metadata)) {
+        ps <- phyloseq(
+            otu_table(table, taxa_are_rows = FALSE),
+            tax_table(taxa)
+        )
+        return(ps)
+    }
+
+    metadata <- as.data.frame(metadata)
+    rownames(metadata) <- metadata[[id_col]]
+    ps <- phyloseq(
+        otu_table(table, taxa_are_rows = FALSE),
+        tax_table(taxa),
+        sample_data(metadata)
+    )
+    return(ps)
+}
+
+
+#' Converts read count data to a phyloseq object.
+#'
+#' @param counts A data.table containing at least columns "reads",
+#'   "sample_id", and features.
+#' @param metadata A metadata in data.table format to use.
+#' @param feature_col Column in the count table containing the features.
+#' @param id_col Column in the metadata corresponding to the sample IDs
+#' in the read counts.
+#' @return A phyloseq object for the data.
+#' @examples
+#'  NULL
+#'
+#' @export
+reads_to_phyloseq <- function(
+    counts,
+    feature_col,
+    metadata = NULL,
+    id_col = "sample_id") {
+
+    table <- dcast(
+        counts,
+        reformulate(feature_col, response="sample_id"),
+        value.var = "reads",
+        fill = 0
+    )
+    samps <- table[, sample_id]
+    table <- as.matrix(table[, !"sample_id"])
+    rownames(table) <- samps
+    features <- matrix(colnames(table), ncol = 1)
+    rownames(features) <- features[, 1]
+    colnames(features) <- feature_col
+
+    if (is.null(metadata)) {
+        ps <- phyloseq(
+            otu_table(table, taxa_are_rows = FALSE),
+            tax_table(features)
+        )
+        return(ps)
+    }
+
+    metadata <- as.data.frame(metadata)
+    rownames(metadata) <- metadata[[id_col]]
+    ps <- phyloseq(
+        otu_table(table, taxa_are_rows = FALSE),
+        tax_table(features),
+        sample_data(metadata)
+    )
+    return(ps)
+
+    }
